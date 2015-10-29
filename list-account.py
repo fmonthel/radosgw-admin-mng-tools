0fc7c328d313809bd92fc51b704d5d813870ccb6
fmonthel <fmonthel@flox-arts.net>
2015-10-28 23:36:03 -0400

    Add  number of objects per account

--- list-account.py
+++ list-account.py
@@ -41,7 +41,9 @@ for user in users:
 		gb_received = 0
 		gb_sent = 0
 		nb_ops = 0
-	kb = 0
+	# Global usage
+    kb_total = 0
+    obj_total = 0
 	# Print summary
 	print "Stats - Nb ops : %d - Downloaded data : %.2f GB - Uploaded data : %.2f GB" % (nb_ops, gb_sent, gb_received)
 	# Get buckets of user
@@ -52,20 +54,24 @@ for user in users:
 		if dBucket["bucket_quota"]["enabled"] == False :
 			# Get values
 			if('rgw.main' in dBucket["usage"].keys()):
-				kb = kb + dBucket["usage"]["rgw.main"]["size_kb"]
 				nb_object = dBucket["usage"]["rgw.main"]["num_objects"]
 				size_gb = float(dBucket["usage"]["rgw.main"]["size_kb"])/1048576
+                # Global value
+                kb_total = kb_total + dBucket["usage"]["rgw.main"]["size_kb"]
+                obj_total = obj_total + dBucket["usage"]["rgw.main"]["num_objects"]
 			else :
 				nb_object = 0
 				size_gb = 0
 			# Print values
-			print "- Bucket : %s (quota disabled) - Nb objects : %d - GB data : %.2f" % (bucket, nb_object, size_gb)
+			print "- Bucket : %s (quota disabled) - Nb object(s) : %d - GB data : %.2f" % (bucket, nb_object, size_gb)
 		elif dBucket["bucket_quota"]["enabled"] == True :
 			# Get values
 			if('rgw.main' in dBucket["usage"].keys()):
-				kb = kb + dBucket["usage"]["rgw.main"]["size_kb"]
 				nb_object = dBucket["usage"]["rgw.main"]["num_objects"]
 				size_gb = float(dBucket["usage"]["rgw.main"]["size_kb"])/1048576
+                # Global value
+                kb_total = kb_total + dBucket["usage"]["rgw.main"]["size_kb"]
+                obj_total = obj_total + dBucket["usage"]["rgw.main"]["num_objects"]
 			else :
 				nb_object = 0
 				size_gb = 0
@@ -78,5 +84,5 @@ for user in users:
 			else :
 				quota_gb = float(dBucket["bucket_quota"]["max_size_kb"])/1048576
 			# Print values
-			print "- Bucket : %s (quota enabled) - Nb objects : %d (quota : %d) - GB data : %.2f (quota : %.2f GB)" % (bucket, nb_object, quota_object, size_gb, quota_gb)
-	print "Usage summary for account %.2fGB\n" % (float(kb)/1048576)
+			print "- Bucket : %s (quota enabled) - Nb object(s) : %d (quota : %d) - GB data : %.2f (quota : %.2f GB)" % (bucket, nb_object, quota_object, size_gb, quota_gb)
+	print "Usage summary for account %.2fGB with %d object(s)\n" % (float(kb_total)/1048576,obj_total)
